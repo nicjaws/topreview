4c43fd46485d2478be5f5850ff6ec64bfca4c182
Nicolas Corbalan <outreef@hotmail.com>
2019-09-24 00:47:11 +0200

    register screen

--- app/screens/MyAccount/Register.js
+++ app/screens/MyAccount/Register.js
@@ -2,15 +2,32 @@ import React, {Component} from "react";
 import {StyleSheet, View, Text} from "react-native";
 
 import t from 'tcomb-form-native'; 
-const Form = t.form.Form;
-import { RegisterStruct, RegisterOptions } from "../../forms/Register";
+const Form = t.form.Form; import { RegisterStruct, RegisterOptions } from "../../forms/Register";
 
 
 export default class Register extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            registerStruct: RegisterStruct,
+            registerOptions: RegisterOptions
+        }
+    }
     render() {
+
+        const { registerStruct, registerOptions } = this.state
+
         return (
             <View style={styles.viewBody}>
                 <Text>Register Screen...</Text>
+
+                <Form
+                    ref="registerForm"
+                    type={registerStruct}
+                    options={registerOptions}
+                />
+>
             </View>
         )
     }
